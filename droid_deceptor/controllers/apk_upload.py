16034973407a60896a520bc3d51b028e3b695ac4
Moghees244 <mogheesahmad244@gmail.com>
2023-10-07 19:33:33 +0500

    Template Inheritance and Apk Upload with MVC Architecture

---
+++ droid_deceptor/controllers/apk_upload.py
@@ -0,0 +1,28 @@
+import os
+from flask import render_template
+
+def get_apk(files):
+    UPLOAD_FOLDER = 'droid_deceptor/uploads'
+
+    success_message, error_message , uploaded_file = None, None, None
+
+    # Check if the 'file' field is in the request
+    if 'file' not in files:
+        error_message = "No file Uploaded"
+
+    file = files['file']
+
+    # Check if the user submitted an empty file input
+    if file.filename == '':
+        error_message = "No selected file"
+
+    if file:
+        # Ensure the folder exists
+        os.makedirs(UPLOAD_FOLDER, exist_ok=True)
+
+        # Save the uploaded APK file to the UPLOAD_FOLDER
+        file.save(os.path.join(UPLOAD_FOLDER, file.filename))
+        uploaded_file = file.filename
+        success_message = "File Saved"
+
+    return render_template('upload.html', success_message=success_message, error_message=error_message, uploaded_file=uploaded_file)
